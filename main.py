1950fca2af5503e74156ff39bcb39867b981b775
RafaelJMartini <rafa.martini1102@gmail.com>
2025-04-13 15:44:41 -0300

    Add the HTML files, and created the Forms classes

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World'
+lista_usuarios = ['Rafael','João','Martini','Ronaldo']
 
+app.config['SECRET_KEY'] = ''
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios = lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
