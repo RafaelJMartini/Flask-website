deeb09d700306eaea09887d8f477f85a205c6e86
RafaelJMartini <rafa.martini1102@gmail.com>
2025-04-20 22:35:56 -0300

    Created the form to update profile pic

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField,PasswordField,SubmitField, BooleanField
 from wtforms.validators import DataRequired,Email,Length, EqualTo,ValidationError
 from comunidadeimpressionadora.models import Usuario
@@ -28,6 +29,7 @@ class FormLogin(FlaskForm):
 class FormEditarPerfil(FlaskForm):
      username  = StringField('Username',validators=[DataRequired()])
      email = StringField('E-mail', validators=[DataRequired(), Email()])
+     foto_perfil = FileField('Atualizar foto de perfil', validators=[FileAllowed(['jpg','png'])])
      botao_submit_editar_perfil = SubmitField('Confirmar Edição')
 
      def validate_email(self,email):
