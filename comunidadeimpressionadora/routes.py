2c3908bcbf4cda10697fb8e9c840f258ae52d165
RafaelJMartini <rafa.martini1102@gmail.com>
2025-05-03 12:49:40 -0300

    Feature delete post now works properly

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -2,7 +2,7 @@ from fileinput import filename
 from comunidadeimpressionadora import app,database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora.models import Usuario, Post
-from flask import render_template, redirect, url_for,flash,request
+from flask import render_template, redirect, url_for,flash,request, abort
 from flask_login import login_user, logout_user, current_user, login_required
 import secrets
 import os
@@ -148,3 +148,15 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html',post=post, form=form)
+
+@app.route('/post/<post_id>/excluir', methods=["GET","POST"])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post excluído com sucesso!', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
