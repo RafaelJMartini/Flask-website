8d92c4e045cb9d24ef57c31242b8e44b365aaf14
RafaelJMartini <rafa.martini1102@gmail.com>
2025-05-07 17:51:09 -0300

    Changed the way the program create the database

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import sqlalchemy
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 
 load_dotenv()
@@ -20,5 +20,15 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de dados criada com sucesso')
+else:
+    print('Base de dados já existente')
 
 from comunidadeimpressionadora import routes
